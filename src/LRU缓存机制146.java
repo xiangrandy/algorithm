3748798a0377ae42712e8bc20d368b270882da7e
x00418543 <x00418543@LAPTOP-J2OH49EU.china.huawei.com>
2020-02-27 09:42:21 +0800

    外观数列

--- src/LRU缓存机制146.java
+++ src/LRU缓存机制146.java
@@ -1,23 +1,13 @@
-/*
- * Copyright (c) Huawei Technologies Co., Ltd. 2015-2020. All rights reserved.
- */
-
-/**
- * LRU缓存机制
- * 
- * @author x00418543
- * @since 2020年1月17日
- */
 public class LRU缓存机制146 {
 
-    public static void main(String[] args) {
-        LRUCache cache = new LRUCache(2 /* 缓存容量 */ );
-        cache.put(2, 1);
-        cache.put(1, 2);
-        cache.put(2, 3);
-        cache.put(4, 1);
-        System.out.println(cache.get(1));
-        System.out.println(cache.get(2));
-    }
+	public static void main(String[] args) {
+		LRUCache cache = new LRUCache(2 /* 缓存容量 */ );
+		cache.put(2, 1);
+		cache.put(1, 2);
+		cache.put(2, 3);
+		cache.put(4, 1);
+		System.out.println(cache.get(1));
+		System.out.println(cache.get(2));
+	}
 
 }
